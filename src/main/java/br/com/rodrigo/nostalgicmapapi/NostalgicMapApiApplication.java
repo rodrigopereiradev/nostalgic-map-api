15cfd9cc5cb7833fcdd6cbc16a643c61858811a4
Rodrigo S. Pereira <rodrigo.pereira@aisdigital.com.br>
2018-11-17 19:19:18 -0200

    my first commit

---
+++ src/main/java/br/com/rodrigo/nostalgicmapapi/NostalgicMapApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.rodrigo.nostalgicmapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NostalgicMapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NostalgicMapApiApplication.class, args);
+	}
+}
